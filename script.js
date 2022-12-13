8ec0b7f071c88e89c1589b76efb3c10a0269c1e2
Veis1337 <117036853+Veis1337@users.noreply.github.com>
2022-12-13 12:52:39 -0500

    Added timer, scores, still need to add high scores with Local storage

--- script.js
+++ script.js
@@ -5,18 +5,52 @@ let objectAnswer2 = document.getElementById("answer2");
 let objectAnswer3 = document.getElementById("answer3");
 let objectAnswer4 = document.getElementById("answer4");
 let playNow = document.getElementById("startButton");
-playNow.addEventListener("click", beginQuiz);
+let tallyTotal = document.getElementById("tally");
+let currentScore = document.getElementById("currentScore");
+let highScores = document.getElementById("highScores");
+let timer = document.getElementById("timer");
+let answersList = document.getElementById("answersList");
+
+highScores.addEventListener("click", scoresChart());
+playNow.addEventListener("click", () => {
+    beginQuiz();
+    countdown();
+});
+
+//variables to count
+let i = 0;
+let win = 0;
+let lose = 0;
+let timeLeft = 0;
+let score = 0;
+let key = "";
+
+document.addEventListener('keydown', function (event) {
+    if (timeLeft === 0) {
+        return;
+    }
+    // Access value of pressed key with key property
+    key = event.key.toLowerCase();
+    let abcd = "abcd".split("");
+
+    if (abcd.includes(key)) {
+        console.log(key);
+        checkInput();
+    }
+
+});
+
 
 
 //create an object for each question
 let questionObjects = [
     {
-        question: "first question",
-        answer1: "A",
-        answer2: "B",
-        answer3: "C",
-        answer4: "D",
-        correctAnswer: "A"
+        question: "Commonly used data types does NOT include which of the following?",
+        answer1: "Alerts",
+        answer2: "Strings",
+        answer3: "Booleans",
+        answer4: "Numbers",
+        correctAnswer: "a"
     },
     {
         question: "second question",
@@ -24,7 +58,7 @@ let questionObjects = [
         answer2: "B",
         answer3: "C",
         answer4: "D",
-        correctAnswer: "B"
+        correctAnswer: "b"
     },
     {
         question: "third question",
@@ -32,7 +66,7 @@ let questionObjects = [
         answer2: "B",
         answer3: "C",
         answer4: "D",
-        correctAnswer: "C"
+        correctAnswer: "c"
     },
     {
         question: "fourth question",
@@ -40,7 +74,7 @@ let questionObjects = [
         answer2: "B",
         answer3: "C",
         answer4: "D",
-        correctAnswer: "D"
+        correctAnswer: "d"
     },
     {
         question: "fifth question",
@@ -48,42 +82,62 @@ let questionObjects = [
         answer2: "B",
         answer3: "C",
         answer4: "D",
-        correctAnswer: "A"
+        correctAnswer: "a"
     }
 ]
-
-//Create a function that will pull a random object from questionObjects,
-//and append/replace the attributes to their respective HTML elements on the DOM
+//Function to begin quiz and change DOM initially
 function beginQuiz() {
-    for (i = 0; i < questionObjects.length; i++) {
-        currentQuestion.innerHTML = questionObjects[i].question;
-        objectAnswer1.innerHTML = questionObjects[i].answer1;
-        objectAnswer2.innerHTML = questionObjects[i].answer2;
-        objectAnswer3.innerHTML = questionObjects[i].answer3;
-        objectAnswer4.innerHTML = questionObjects[i].answer4;
-        let userInput = window.prompt("A, B, C, or D?");
-            if (userInput == questionObjects[i].correctAnswer) {
-                console.log("Correct");
-            } else {
-                console.log("Incorrect");
-            }
-    } 
+    answersList.style = "visibility: hidden;"
+    if (i === questionObjects.length) {
+        alert("That's all, folks!");
+        reportResults();
+        return;
+    }
+    tallyTotal.innerHTML = "Correct: " + win + "  Incorrect: " + lose;
+    playNow.innerHTML = "Think you know it? Hit the key that corresponds to your answer! (abcd)";
+    currentQuestion.innerHTML = questionObjects[i].question;
+    objectAnswer1.innerHTML = questionObjects[i].answer1;
+    objectAnswer2.innerHTML = questionObjects[i].answer2;
+    objectAnswer3.innerHTML = questionObjects[i].answer3;
+    objectAnswer4.innerHTML = questionObjects[i].answer4;
+}
+function checkInput() {
+    score = 5000 / timeLeft * win;
+    currentScore.innerHTML = "Your Score is " + Math.floor(score);
+    if (key == questionObjects[i].correctAnswer) {
+        alert("Correct!");
+        win = win + 1;
+    } else {
+        alert("Incorrect, you'll get 'em next time...");
+        lose = lose + 1;
+    } i = i + 1; beginQuiz();
 }
 
+function reportResults() {
+    console.log(win);
+    console.log(lose);
+    console.log(timeLeft);
+}
 
-// for (let i = 0; i < questionObjects.length; i++){
-//     currentQuestion.innerHTML = questionObjects[i].question;
-//     console.log(i);
-// }
+function countdown() {
+    timeLeft = 180;
+    let timeInterval = setInterval(function () {
+        timeLeft--;
+        timer.textContent = timeLeft + " seconds remaining."
+        if (timeLeft === 0) {
+            clearInterval(timeInterval);
+            timer.textContent = "";
+            alert("Time's Up!")
+            reportResults();
+        } else if (i === questionObjects.length) {
+            clearInterval(timeInterval);
+        }
+    }, 1000);
+}
 
+function scoresChart() {
 
-
-
-
-
-
-
-
+}
 
 
 // GIVEN I am taking a code quiz
